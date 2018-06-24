0895252732fbd1ed2609b2fa74bcb3ca93974f31
Rodrigo Fernandes <rtfrodrigo@gmail.com>
2018-06-24 22:56:22 +0100

    Refactor to use new API

---
+++ project/Dependencies.scala
@@ -0,0 +1,8 @@
+import sbt._
+
+object Dependencies {
+
+  val seed = ("com.codacy" %% "codacy-engine-scala-seed" % "0.1.0-pre.refactor-SNAPSHOT").withSources() //"2.7.9").withSources()
+  val scalaXml = ("org.scala-lang.modules" %% "scala-xml" % "1.1.0").withSources()
+
+}
